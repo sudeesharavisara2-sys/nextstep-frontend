5db46cb889b0bc2b7a2cf3a70242036db4eac783
sudeesharavisara2-sys <sudeesharavisara2@gmail.com>
2026-01-26 11:06:58 +0530

    Clean up comments in API interceptor
    Remove commented code in src/api.js

--- src/api.js
+++ src/api.js
@@ -4,9 +4,9 @@ const API = axios.create({
     baseURL: 'http://localhost:8099/api/v1',
 });
 
-// සෑම Request එකකටම Token එක ස්වයංක්‍රීයව එකතු කිරීමට (Interceptor)
+
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token'); // Login එකේදී save කළ token එක ලබා ගැනීම
+    const token = localStorage.getItem('token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
